1bfd72e25696f8556c4eefb3e9e223fabe5ff49b
Realfair stanley <realfair95@gmail.com>
2018-10-21 01:54:18 +0200

    Remove Spinner for Colors

--- public/public_web.php
+++ public/public_web.php
@@ -1048,21 +1048,6 @@ require $root_url;
 	include_once 'App/Views/Utils/subscribe_modal.php';
 	?>
 	
-	<!--/Preset Style Chooser--> 
-	<div class="style-chooser">
-		<div class="style-chooser-inner">
-			<a href="#" class="toggler"><i class="fa fa-life-ring fa-spin"></i></a>
-			<h4>Presets Color</h4>
-			<ul class="preset-list clearfix">
-				<li class="preset1 active" data-preset="1"><a href="#" data-color="preset1"></a></li>
-				<li class="preset2" data-preset="2"><a href="#" data-color="preset2"></a></li>
-				<li class="preset3" data-preset="3"><a href="#" data-color="preset3"></a></li>        
-				<li class="preset4" data-preset="4"><a href="#" data-color="preset4"></a></li>
-				<li class="preset5" data-preset="5"><a href="#" data-color="preset5"></a></li>
-				<li class="preset6" data-preset="6"><a href="#" data-color="preset6"></a></li>
-			</ul>
-		</div>
-    </div>
     <?php 
     include 'App/Views/Home/script.php';
     ?>
